8251de5bd1109f9f931fe807f8833900473cfe6f
dvcalex <115949328+dvcalex@users.noreply.github.com>
2025-09-17 16:43:39 -0400

    testing

--- js/main3D.js
+++ js/main3D.js
@@ -1,31 +1,69 @@
 ﻿import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer';
 
-// ------------------- scene boiler plate -------------------
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Sets the color of the background.
+renderer.setClearColor(0xFEFEFE);
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.set(0, 1, 8);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-// ------------------- scene boiler plate -------------------
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.update();
+
+// ----------------------------------- RENDER BELOW -----------------------------------
+
+const dLight = new THREE.DirectionalLight(0xffffff, 1);
+dLight.position.set(-3, 10, 0);
+scene.add(dLight);
+
+const light = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(light);
+
+const sphereGeometry = new THREE.SphereGeometry(1.9, 50, 50);
+const sphereMaterial = new THREE.MeshPhongMaterial({ color: 'lime' });
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphere);
+sphere.position.y = -0.5;
+
+// ----------------------------------- RENDER ABOVE -----------------------------------
+
+// CSS2D container
+const labelRenderer = new CSS2DRenderer();
+labelRenderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(labelRenderer.domElement);
+
+// styling
+labelRenderer.domElement.style.position = 'absolute';
+labelRenderer.domElement.style.top = '0px';
+labelRenderer.domElement.style.pointerEvents = 'none';
 
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5;
-
-
-// ------------------- animation handling -------------------
-function animate() {
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    requestAnimationFrame(animate);
+function animate()
+{
+    labelRenderer.render(scene, camera);
     renderer.render(scene, camera);
 }
-animate();
-// ------------------- animation handling -------------------
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function ()
+{
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    labelRenderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
